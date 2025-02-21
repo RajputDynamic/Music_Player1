d507351c1765c550a246febf4fdd0c09cc14813d
rajput11dynamic <rajput11dynamic@gmail.com>
2025-02-21 18:19:52 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import  'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme:  ThemeData(brightness: Brightness.light),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.system,
+      title: "QR Scanner & Generator",color: Colors.deepOrange,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+
+}
